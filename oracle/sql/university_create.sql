775913ad2c152b0dba06ca417f8f64cb8b4e22ac
martinkarbon <martin.karbon16@gmail.com>
2019-10-28 17:47:05 +0100

    SQL server + Oracle image update

--- oracle/sql/university_create.sql
+++ oracle/sql/university_create.sql
@@ -14,10 +14,23 @@ insert into "SYSTEM".professors (prof_id, first_name, last_name) values (6, 'Joh
 insert into "SYSTEM".professors (prof_id, first_name, last_name) values (7, 'Barbara', 'Dodero');
 insert into "SYSTEM".professors (prof_id, first_name, last_name) values (8, 'Mary', 'Poppins');
 
+create table "SYSTEM".course (
+	course_id VARCHAR2(100) primary key,
+	nb_students NUMBER NOT NULL,
+	duration decimal (10, 3) NOT NULL
+);
+
+insert into "SYSTEM".course (course_id, nb_students, duration) values ('LinearAlgebra', 10, 24.5);
+insert into "SYSTEM".course (course_id, nb_students, duration) values ('DiscreteMathematics', 11, 30);
+insert into "SYSTEM".course (course_id, nb_students, duration) values ('AdvancedDatabases', 12, 20);
+insert into "SYSTEM".course (course_id, nb_students, duration) values ('ScientificWriting', 13, 18);
+
 create table "SYSTEM".teaching (
 	course_id VARCHAR2(100) NOT NULL,
 	prof_id NUMBER NOT NULL,
-	foreign key (prof_id) REFERENCES "SYSTEM".professors(prof_id)
+	primary key (course_id, prof_id),
+	foreign key (prof_id) REFERENCES "SYSTEM".professors(prof_id),
+	foreign key (course_id) REFERENCES "SYSTEM".course(course_id)
 );
 
 insert into "SYSTEM".teaching (course_id, prof_id) values ('LinearAlgebra', 1);
