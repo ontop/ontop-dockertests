26ddeca473cb11f58bc4155e255da9f0fc984bf3
juliencorman <cormanjulien@gmail.com>
2020-06-01 08:59:19 +0200

    Added data to test distinct within aggregation functions

--- oracle/sql/university_create.sql
+++ oracle/sql/university_create.sql
@@ -24,6 +24,7 @@ insert into "SYSTEM".course (course_id, nb_students, duration) values ('LinearAl
 insert into "SYSTEM".course (course_id, nb_students, duration) values ('DiscreteMathematics', 11, 30);
 insert into "SYSTEM".course (course_id, nb_students, duration) values ('AdvancedDatabases', 12, 20);
 insert into "SYSTEM".course (course_id, nb_students, duration) values ('ScientificWriting', 13, 18);
+insert into "SYSTEM".course (course_id, nb_students, duration) values ('OperatingSystems', 10, 30);
 
 create table "SYSTEM".teaching (
 	course_id VARCHAR2(100) NOT NULL,
@@ -37,3 +38,4 @@ insert into "SYSTEM".teaching (course_id, prof_id) values ('LinearAlgebra', 1);
 insert into "SYSTEM".teaching (course_id, prof_id) values ('DiscreteMathematics', 1);
 insert into "SYSTEM".teaching (course_id, prof_id) values ('AdvancedDatabases', 3);
 insert into "SYSTEM".teaching (course_id, prof_id) values ('ScientificWriting', 8);
+insert into "SYSTEM".teaching (course_id, prof_id) values ('OperatingSystems', 1);
