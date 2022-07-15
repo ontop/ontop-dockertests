3084fc15ce257f6cff69e9b8ec9520c0e4426f33
apano-on <albulen.pano@unibz.it>
2022-07-15 17:24:19 +0200

    Add university minidb

---
+++ db2/core-sql/university-db2.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE UNIVERSITY;
+
+CONNECT TO UNIVERSITY;
+
+create table professors (
+	prof_id int primary key NOT NULL,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+  nickname varchar(100)
+);
+
+insert into professors (prof_id, first_name, last_name, nickname) values (1, 'Roger', 'Smith', 'Rog');
+insert into professors (prof_id, first_name, last_name, nickname) values (2, 'Frank', 'Pitt', 'Frankie');
+insert into professors (prof_id, first_name, last_name, nickname) values (3, 'John', 'Depp', 'Johnny');
+insert into professors (prof_id, first_name, last_name, nickname) values (4, 'Michael', 'Jackson', 'King of Pop');
+insert into professors (prof_id, first_name, last_name) values (5, 'Diego', 'Gamper');
+insert into professors (prof_id, first_name, last_name) values (6, 'Johann', 'Helmer');
+insert into professors (prof_id, first_name, last_name) values (7, 'Barbara', 'Dodero');
+insert into professors (prof_id, first_name, last_name) values (8, 'Mary', 'Poppins');
+
+create table course (
+	course_id varchar(100) primary key NOT NULL,
+	nb_students int NOT NULL,
+	duration decimal(10, 3) NOT NULL
+);
+
+insert into course (course_id, nb_students, duration) values ('LinearAlgebra', 10, 24.5);
+insert into course (course_id, nb_students, duration) values ('DiscreteMathematics', 11, 30);
+insert into course (course_id, nb_students, duration) values ('AdvancedDatabases', 12, 20);
+insert into course (course_id, nb_students, duration) values ('ScientificWriting', 13, 18);
+insert into course (course_id, nb_students, duration) values ('OperatingSystems', 10, 30);
+
+create table teaching (
+	course_id varchar(100) NOT NULL,
+	prof_id int NOT NULL,
+	primary key (course_id, prof_id),
+	foreign key (prof_id) REFERENCES professors(prof_id),
+	foreign key (course_id) REFERENCES course(course_id)
+);
+
+insert into teaching (course_id, prof_id) values ('LinearAlgebra', 1);
+insert into teaching (course_id, prof_id) values ('DiscreteMathematics', 1);
+insert into teaching (course_id, prof_id) values ('AdvancedDatabases', 3);
+insert into teaching (course_id, prof_id) values ('ScientificWriting', 8);
+insert into teaching (course_id, prof_id) values ('OperatingSystems', 1);
+
+COMMIT WORK;
+
+CONNECT RESET;
+
+TERMINATE;
+
