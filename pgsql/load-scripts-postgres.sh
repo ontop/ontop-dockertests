6b25c35cfe5bf2597b942ee5db21639a86016a39
juliencorman <cormanjulien@gmail.com>
2023-01-16 13:58:11 +0100

    upgrading postgres to 15.1

--- pgsql/load-scripts-postgres.sh
+++ pgsql/load-scripts-postgres.sh
@@ -4,6 +4,6 @@ set -e
 for f in /tmp/sql_scripts/*.dump;
  do
  echo Running $f;
- pg_restore --no-owner -C -d postgres  $f;
+ pg_restore --schema=public --no-owner -C -d postgres  $f;
  echo "DATABASE $f CREATED!"
  done
